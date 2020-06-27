89f285a173062dbbb45ea15d4b76b07ce622f5b3
Software Development 3line <softwaredev@3lineng.com>
2020-06-27 04:47:36 +0100

    added core classes eg. abstract entity,exceptions,utilities

---
+++ src/main/java/com/mintbank/cardsystem/core/entity/AbstractEntity.java
@@ -0,0 +1,95 @@
+package com.mintbank.cardsystem.core.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+
+import javax.persistence.*;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@MappedSuperclass
+
+public abstract class AbstractEntity implements Serializable, SerializableEntity<AbstractEntity> {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    Long id;
+
+
+    protected final Date createdOn = new Date();
+
+    private Date updateOn;
+
+
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractEntity other = (AbstractEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String serialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String data = mapper.writeValueAsString(this);
+        return data;
+    }
+
+    @Override
+    public void deserialize(String data) throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        AbstractEntity readValue = mapper.readValue(data, this.getClass());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(readValue, this);
+
+    }
+
+    @JsonIgnore
+    public List<String> getDefaultSearchFields(){
+        return new ArrayList<String>();
+    };
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "id=" + id +
+                ", createdOn=" + createdOn +
+                ", updateOn=" + updateOn +
+                '}';
+    }
+}
